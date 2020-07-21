d6a868d08495d0e48382cfd8da79cb853271b039
deasonfun <deasonfun@yahoo.com>
2020-07-21 03:25:38 -0400

    Added zsh config and install

--- install.sh
+++ install.sh
@@ -6,4 +6,8 @@ cp starship.toml ~/.config/
 apt install python3-dev python3-pip python3-setuptools
 pip3 install thefuck
 
+sudo apt install zsh
+chsh -s /bin/zsh seeni
+cp .zshrc ~/
+
 cp .bashrc ~/
