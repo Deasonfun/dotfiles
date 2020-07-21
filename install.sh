9371e3ff593848d04bfb27aa9075d2185e11cab8
deasonfun <deasonfun@yahoo.com>
2020-07-21 03:28:46 -0400

    Added comments to installer

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 apt update
 
+#Starship
 curl -fsSL https://starship.rs/install.sh | bash
 cp starship.toml ~/.config/
 
+#thefuck command
 apt install python3-dev python3-pip python3-setuptools
 pip3 install thefuck
 
+#zsh
 sudo apt install zsh
 chsh -s /bin/zsh seeni
 cp .zshrc ~/
